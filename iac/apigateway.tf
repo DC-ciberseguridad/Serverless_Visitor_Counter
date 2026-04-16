4f3919b94d2e5d9b9e860c2766de95f51a1ae684
Dcardenas206 <danielcardenas206@hotmail.com>
2026-04-16 12:14:16 -0500

    second commit

--- iac/apigateway.tf
+++ iac/apigateway.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_apigatewayv2_api" "lambda_api" {
   name          = "visitor_counter_api"
   protocol_type = "HTTP"
@@ -34,3 +35,4 @@ resource "aws_lambda_permission" "api_gw" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
 }
+*/
--- iac/lambda.tf
+++ iac/lambda.tf
@@ -1,4 +1,4 @@
-resource "aws_lambda_function" "visitor_function" {
+/*resource "aws_lambda_function" "visitor_function" {
   function_name = "visitor_counter_function"
   role          = aws_iam_role.lambda_role.arn
   package_type  = "Image"
@@ -10,3 +10,4 @@ resource "aws_lambda_function" "visitor_function" {
     }
   }
 }
+*/
